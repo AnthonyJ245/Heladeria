5b1afe05d2931340e6e2724a532650478f0f70a0
AnthonyJ123 <anthony.jeres@gmail.com>
2021-07-26 21:58:04 -0500

    Carpeta

--- Heladeria/Trabajador.Designer.cs
+++ Heladeria/Trabajador.Designer.cs
@@ -29,10 +29,17 @@ namespace Heladeria
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Trabajador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(636, 377);
+            this.Name = "Trabajador";
             this.Text = "Trabajador";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
